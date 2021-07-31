506ec954370a4c216c845ca610fe33d285610c69
ethan-bloom19 <ebloom19@gmail.com>
2021-07-31 15:58:58 +0800

    Player choices functionality built

--- script.js
+++ script.js
@@ -9,13 +9,25 @@ for (let selection of board) {
 const piece = document.getElementById('user-choice');
 piece.addEventListener('click', selection);
 
-let userChoice = '';
+let playerOne = '';
+let playerTwo = '';
 function selection() {
     const selectedPiece = document.querySelectorAll('input[name="checker"]');
     for (let i of selectedPiece) {
         if (i.checked) {
-            userChoice = i.value;
-            console.log("User choose " + userChoice);
+            let chooseYourPiece = document.getElementById('player-text');
+            if (playerOne == 'X' || playerOne == 'O') {
+                playerTwo = i.value;
+                console.log("Player 2 choose " + playerTwo);
+                chooseYourPiece.innerText = '';
+                const hideChoice = document.getElementById('select-piece');
+                hideChoice.setAttribute('style', 'display: none;');
+                return;
+            }
+            playerOne = i.value;
+            console.log("Player 1 choose " + playerOne);
+
+            chooseYourPiece.innerText = 'Player 2 choose your piece';
             return;
         }
     }
@@ -27,7 +39,7 @@ function addMove(event) {
     event.target.children[0].classList.add('selected');
 
     let move = event.target.children[0];
-    move.innerText = userChoice; // To put piece element here
+    move.innerText = 'X'; // To put piece element here
 
 
 }
