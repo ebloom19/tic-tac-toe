618a2c087c67436fd57acb739842f13fb5b401a6
ethan-bloom19 <ebloom19@gmail.com>
2021-07-31 15:07:38 +0800

    Added functionality to choose noughts or crosses

--- script.js
+++ script.js
@@ -5,11 +5,29 @@ for (let selection of board) {
     selection.addEventListener('click', addMove);
 }
 
-function addMove(event, piece) {
+
+const piece = document.getElementById('user-choice');
+piece.addEventListener('click', selection);
+
+let userChoice = '';
+function selection() {
+    const selectedPiece = document.querySelectorAll('input[name="checker"]');
+    for (let i of selectedPiece) {
+        if (i.checked) {
+            userChoice = i.value;
+            console.log("User choose " + userChoice);
+            return;
+        }
+    }
+
+}
+
+
+function addMove(event) {
     event.target.children[0].classList.add('selected');
 
     let move = event.target.children[0];
-    move.innerText = 'X'; // To put piece element here
+    move.innerText = userChoice; // To put piece element here
 
 
 }
